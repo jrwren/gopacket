699c8a0dfbe29975165b30f1ce94fc4e5ff9a506
Jay Wren <jawren@cisco.com>
2019-03-06 18:47:07 -0500

    WIP better TLSHandshake

--- layers/tls_handshake.go
+++ layers/tls_handshake.go
@@ -13,6 +13,8 @@ import (
 // TLSHandshakeRecord defines the structure of a Handshare Record
 type TLSHandshakeRecord struct {
 	TLSRecordHeader
+	Type    TLSHandshakeType
+	Payload []byte
 }
 
 // DecodeFromBytes decodes the slice into the TLS struct.
@@ -22,7 +24,25 @@ func (t *TLSHandshakeRecord) decodeFromBytes(h TLSRecordHeader, data []byte, df 
 	t.Version = h.Version
 	t.Length = h.Length
 
-	// TODO
-
+	t.Type = TLSHandshakeType(data[0])
+	t.Payload = data
 	return nil
 }
+
+type TLSHandshakeType uint8
+
+// TLS handshake message types.
+const (
+	TLSHandshakeClientHello        TLSHandshakeType = 1
+	TLSHandshakeServerHello        TLSHandshakeType = 2
+	TLSHandshakeNewSessionTicket   TLSHandshakeType = 4
+	TLSHandshakeCertificate        TLSHandshakeType = 11
+	TLSHandshakeServerKeyExchange  TLSHandshakeType = 12
+	TLSHandshakeCertificateRequest TLSHandshakeType = 13
+	TLSHandshakeServerHelloDone    TLSHandshakeType = 14
+	TLSHandshakeCertificateVerify  TLSHandshakeType = 15
+	TLSHandshakeClientKeyExchange  TLSHandshakeType = 16
+	TLSHandshakeFinished           TLSHandshakeType = 20
+	TLSHandshakeCertificateStatus  TLSHandshakeType = 22
+	TLSHandshakeNextProtocol       TLSHandshakeType = 67 // Not IANA assigned
+)
